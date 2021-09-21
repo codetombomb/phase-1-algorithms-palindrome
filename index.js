ce03bf8e7bd263391fb5f1796419d1886d861f0d
Tom Tobar <tomas.tobar@flatironschool.com>
2021-09-21 12:41:03 -0500

    done.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word[0] !== word[word.length - 1] ? false : true;
 }
 
 /* 
   Add your pseudocode here
+  If first letter and last letter are different, return false.
 */
 
 /*
   Add written explanation of your solution here
+  compare first and last letters.
+  return false if these are different.
+  return true if they are the same.
 */
 
 // You can run `node index.js` to view these console logs
